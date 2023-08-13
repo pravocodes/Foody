6f3597b08bd78f71a25f12ab81661bee871d2fbe
Praveen Kumar <singhpraveen5673@gmail.com>
2023-08-13 16:03:52 +0530

    Register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,44 @@
-export const registerController = (req, res) => {
+import usermodel from "../models/usermodel.js";
+
+export const registerController = async (req, res) => {
   try {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      res.send({
+    const { name, email, password, phone } = req.body;
+    if (!name || !password) {
+      return res.send({
         message: "Enter username or password",
       });
     }
+    if (!phone) {
+      return res.send({
+        message: "Enter Phone number",
+      });
+    }
+    if (!email) {
+      return res.send({
+        message: "Enter email",
+      });
+    }
+
+    const existinguser = await usermodel.findOne({ email });
+
+    if (existinguser) {
+      return res.status(300).send({
+        success: false,
+        message: "User already Registered",
+      });
+    }
+
+    const user = await new usermodel({
+      name,
+      email,
+      password,
+      phone,
+    }).save();
+
     res.status(200).send({
       success: true,
       message: "Registerd successfully",
-      username,
+      user,
     });
   } catch (error) {
     res.status(400).send({
---
+++ models/usermodel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userschema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Users", userschema);
